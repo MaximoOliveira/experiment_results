a70d693d6ffb4a0887ce3a0123f8f429d57d966f
Maximo Oliveira <moliveira@cirrus03.ncg.ingrid.pt>
2021-04-01 08:44:34 +0100

    quixbugs

---
+++ folders/Figra_QUIXBUGS_BUCKETSORT/Figra_QuixBugs_BUCKETSORT_/output_astor/AstorMain-BUCKETSORT-/src/variant-50_f/java_programs/BUCKETSORT.java
@@ -0,0 +1,16 @@
+package java_programs;
+public class BUCKETSORT {
+	public static java.util.ArrayList<java.lang.Integer> bucketsort(java.util.ArrayList<java.lang.Integer> arr, int k) {
+		java.util.ArrayList<java.lang.Integer> counts = new java.util.ArrayList<java.lang.Integer>(java.util.Collections.nCopies(k, 0));
+		for (java.lang.Integer x : arr) {
+			counts.set(x, counts.get(x) + 1);
+		}
+		java.util.ArrayList<java.lang.Integer> sorted_arr = new java.util.ArrayList<java.lang.Integer>(100);
+		int i = 0;
+		for (java.lang.Integer count : counts) {
+			sorted_arr.addAll(java.util.Collections.nCopies(count, i));
+			i++;
+		}
+		return sorted_arr;
+	}
+}
