1d1aa28159449e8cf07022cd0eec6b06983c2d6e
Maximo Oliveira <moliveira@cirrus03.ncg.ingrid.pt>
2021-04-02 13:54:12 +0100

    test

---
+++ folders/Figra_QUIXBUGS_SQRT/Figra_QuixBugs_SQRT_/output_astor/AstorMain-SQRT-/src/variant-178_f/java_programs/SQRT.java
@@ -0,0 +1,10 @@
+package java_programs;
+public class SQRT {
+	public static double sqrt(double x, double epsilon) {
+		double approx = x / 2.0F;
+		while (java.lang.Math.abs((x / approx) - approx) > epsilon) {
+			approx = 0.5F * (approx + (x / approx));
+		} 
+		return approx;
+	}
+}
