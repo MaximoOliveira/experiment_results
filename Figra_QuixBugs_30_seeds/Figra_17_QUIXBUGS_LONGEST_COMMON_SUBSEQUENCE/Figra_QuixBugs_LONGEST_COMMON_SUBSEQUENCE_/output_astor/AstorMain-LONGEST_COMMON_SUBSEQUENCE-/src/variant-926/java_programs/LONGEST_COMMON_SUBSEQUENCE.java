b8cf6d4e005b74bc6268bec281051bac25a5cfe2
Maximo Oliveira <moliveira@cirrus02.ncg.ingrid.pt>
2021-04-17 11:43:51 +0100

    30 seeds on figra, on bugs it didnt solve with seed 28

---
+++ Figra_QuixBugs_30_seeds/Figra_17_QUIXBUGS_LONGEST_COMMON_SUBSEQUENCE/Figra_QuixBugs_LONGEST_COMMON_SUBSEQUENCE_/output_astor/AstorMain-LONGEST_COMMON_SUBSEQUENCE-/src/variant-926/java_programs/LONGEST_COMMON_SUBSEQUENCE.java
@@ -0,0 +1,25 @@
+package java_programs;
+public class LONGEST_COMMON_SUBSEQUENCE {
+
+
+
+
+
+
+
+
+
+
+
+	public static java.lang.String longest_common_subsequence(java.lang.String a, java.lang.String b) {
+		if (a.isEmpty() || b.isEmpty()) {
+			return "";
+		} else if (a.charAt(0) == b.charAt(0)) {
+			return a.charAt(0) + java_programs.LONGEST_COMMON_SUBSEQUENCE.longest_common_subsequence(a.substring(1), 
+
+			b.substring(1));} else { 			java.lang.String fst = java_programs.LONGEST_COMMON_SUBSEQUENCE.longest_common_subsequence(a, b.substring(1));
+			java.lang.String snd = java_programs.LONGEST_COMMON_SUBSEQUENCE.longest_common_subsequence(a.substring(1), b);
+			return fst.length() >= snd.length() ? fst : snd;
+		}
+
+	}}
